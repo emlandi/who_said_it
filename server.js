fc2b4c64e27b05a9093469e40de5450e5e629e25
Tim Mendoza <timmydoza@gmail.com>
2015-11-15 14:16:35 -0800

    add server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+var app = require('express')();
+var mongoose = require('mongoose');
+var quotesRouter = require(__dirname + '/routes/quotesRouter');
+
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/quotes_dev');
+
+app.use('/api', quotesRouter);
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log('server up on port 3000');
+});
