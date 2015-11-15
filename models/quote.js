63af886a0ab63ebcbe8c0f1da72b83ba0dbf3b2a
Tim Mendoza <timmydoza@gmail.com>
2015-11-15 13:57:50 -0800

    consolidate schemas into quote.js

--- models/quote.js
+++ models/quote.js
@@ -1,10 +1,11 @@
 var mongoose = require('mongoose');
 
-var dictatorQuoteSchema = new mongoose.Schema({
+var quoteSchema = new mongoose.Schema({
   person: {type: String, required: true},
   quote: {type: String, required: true},
   correctGuesses: {type: Number, default: 0},
   incorrectGuesses: {type: Number, default: 0}
 });
 
-module.exports = new mongoose.Model('dictatorQuote', dictatorQuoteSchema);
+module.exports.candidateQuote = new mongoose.Model('CandidateQuote', quoteSchema);
+module.exports.dictatorQuote = new mongoose.Model('DictatorQuote', quoteSchema);
