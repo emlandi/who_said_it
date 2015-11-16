8347eb44558a4061e221deb70861d373dea3f7f6
timmydoza <timmydoza@gmail.com>
2015-11-16 15:02:47 -0800

    Update quotesRouter.js

--- routes/quotesRouter.js
+++ routes/quotesRouter.js
@@ -12,13 +12,13 @@ quotesRouter = exports = module.exports = express.Router();
 quotesRouter.get('/', function(req, res) {
   var choice = randInt(0,2);
   quoteChoices[choice].find({}, function(err, data) {
-    if (err) return handleError(err, data);
+    if (err) return handleError(err, res);
     res.json(JSON.stringify(data[randInt(0, data.length)]));
   });
 });
 
 /*Need:
--post route which accepts an ID and 'correct' or 'incorrect' string.  Will increment correctGuesses or incorrectGuesses in database.
+-patch route which accepts an ID and 'correct' or 'incorrect' string.  Will increment correctGuesses or incorrectGuesses in database.
 -get route which returns top 10 misattributed quotes
 -get route which returns top 10 correctly attributed quotes
 
