20921a5756ebb6313a5b6b1a99f25ef1d4d4e80d
Tim Mendoza <timmydoza@gmail.com>
2015-11-16 21:53:52 -0800

    made tons of changes to frontend.  probably should have made smaller commits. oh well.

--- public/app.js
+++ public/app.js
@@ -4,30 +4,36 @@ function getQuote() {
   $.ajax({
     url: 'api/'
   }).done(function(data) {
-    object = JSON.parse(data)
+    object = JSON.parse(data);
     $('#quote').html('"' + object.quote + '"');
-  })
-};
-//left button clicked adds border & shows pic/name
-$('#person1').click(function(){
-  $(this).attr("class", "clicked");
-  $('#person2').attr("class", "");
+    console.log(data);
+  });
+
+}
+
+function makeChoice(choice) {
   $('#answer').html(object.person);
-  $('#answer').append('<br><img src="/imgs/' + object.person + '.jpg">');
+  $('#answer').append('<br><img src="/imgs/' + object.person + '.jpg" height="300" width="300">');
+  if (choice === object.category) {
+      $('#answer').append('<br>Your guess was correct!');
+  } else {
+      $('#answer').append('<br>Your guess was incorrect.');
+  }
+
+  $('#answer').append
+  $('#overlay').fadeIn(400);
+
+}
+
+$('.choice').click(function() {
+  makeChoice(this.id);
+  console.log(this.id);
 });
-//right button clicked adds border & shows pic/name
-$('#person2').click(function(){
-  $(this).attr("class", "clicked");
-  $('#person1').attr("class", "");
-  $('#answer').html(object.person);
-  $('#answer').append('<br><img src="/imgs/' + object.person + '.jpg">');
-});
-//next button clicked removes border/pic/name and gets new quote
+
 $('#next').click(function(){
-  getQuote();
   $('#answer').html("");
-  $('#person1').attr("src", "").attr("class", "");
-  $('#person2').attr("src", "").attr("class", "");
+  $('#overlay').fadeOut();
+  getQuote();
 });
 
 getQuote();
