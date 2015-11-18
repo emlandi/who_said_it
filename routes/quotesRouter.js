888cb61eb2dcb794fcc13b964380a9bdfed161c2
Emily Landi <emily.landi@gmail.com>
2015-11-18 13:40:48 -0800

    commit before merge

--- routes/quotesRouter.js
+++ routes/quotesRouter.js
@@ -17,22 +17,42 @@ quotesRouter.get('/', function(req, res) {
   });
 });
 
-quotesRouter.post('/', bodyParser.text(), function (req, res){
-  console.log(req.body);
-  debugger;
+//POST route which accepts an ID and 'correct' or 'incorrect' string.  Will increment correctGuesses or incorrectGuesses in database.
+quotesRouter.post('/', function (req, res){
+  req.on('data', function(data) {
+    var objAns = JSON.parse(data.toString());
+    console.log('answer: ' + objAns.answer);
+    console.log('id: ' + objAns.quoteObj._id);
+    // console.log('correct before: ' + objAns.quoteObj.correctGuesses);
+    // console.log('incorrect before: ' + objAns.quoteObj.incorrectGuesses);
+    if (objAns.answer === 'correct') {
+      objAns.quoteObj.find({_id: req.params.id}).update(function(err, update) {
+        if (err) return handleError(err, res);
+        update = objAns.quoteObj.correctGuesses++;
+        res.json({msg: 'The number of correct guesses is: ' + update});
+      });
 
+      // console.log('correct after: ' + objAns.quoteObj.correctGuesses);
+      // console.log('incorrect after: ' + objAns.quoteObj.incorrectGuesses);
+    };
+    if (objAns.answer === 'incorrect') {
+      objAns.quoteObj.find({_id: req.params.id}).update(function(err, update) {
+        if (err) return handleError(err, res);
+        update = objAns.quoteObj.incorrectGuesses++;
+        res.json({msg: 'The number of correct guesses is: ' + update});
+      });
+      // console.log('correct after: ' + objAns.quoteObj.correctGuesses);
+      // console.log('incorrect after: ' + objAns.quoteObj.incorrectGuesses);
+    };
+  });
 });
 
+// GET route which returns top 10 misattributed quotes
 quotesRouter.get('/', function(req, res){
 
 });
 
+// GET route which returns top 10 correctly attributed quotes
 quotesRouter.get('/', function(req, res){
 
 });
-
-/* Need:
--patch route which accepts an ID and 'correct' or 'incorrect' string.  Will increment correctGuesses or incorrectGuesses in database.
--get route which returns top 10 misattributed quotes
--get route which returns top 10 correctly attributed quotes
-*/
