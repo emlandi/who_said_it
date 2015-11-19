d2e294f6e99e8ca3ef75721103c88646e6bea815
Emily Landi <emily.landi@gmail.com>
2015-11-19 11:17:49 -0800

    commit before merge

--- routes/quotesRouter.js
+++ routes/quotesRouter.js
@@ -34,16 +34,10 @@ quotesRouter.post('/', bodyParser.json(), function (req, res){
   }
 });
 
-quotesRouter.get('/test', function(req, res) {
-  DictatorQuote.aggregate([{ $sort: {correctGuesses: -1}}], function(err, data) {
+quotesRouter.get('/test', function(req, res, next) {
+  DictatorQuote.aggregate([{ $sort: {correctGuesses: -1}}], [{ $sort: {incorrectGuesses: -1}}], function(err, data) {
     if (err) return handleError(err, res);
     res.json(data.slice(0, 5));
-    //This info is correct. Need to get it to populate in stats.html via stats.js
-    console.log(data[0].quote + "  " + data[0].person + "  " + data[0].correctGuesses);
-    console.log(data[1].quote + "  " + data[1].person + "  " + data[1].correctGuesses);
-    console.log(data[2].quote + "  " + data[2].person + "  " + data[2].correctGuesses);
-    console.log(data[3].quote + "  " + data[3].person + "  " + data[3].correctGuesses);
-    console.log(data[4].quote + "  " + data[4].person + "  " + data[4].correctGuesses);
   });
 
   // CandidateQuote.aggregate([{ $sort: {correctGuesses: -1}}], function(err, data) {
