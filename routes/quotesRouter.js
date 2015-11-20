2eebb33cec9697332270f72f6f82cdcf17bfde8c
Tim Mendoza <timmydoza@gmail.com>
2015-11-20 10:10:21 -0800

    clean up tests

--- routes/quotesRouter.js
+++ routes/quotesRouter.js
@@ -2,6 +2,7 @@ var express = require('express');
 var CandidateQuote = require(__dirname + '/../models/candidateQuote');
 var DictatorQuote = require(__dirname + '/../models/dictatorQuote');
 var quoteChoices = [CandidateQuote, DictatorQuote];
+var choiceKey = {'Candidate': CandidateQuote, 'Dictator': DictatorQuote};
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var handleError = require(__dirname + '/../lib/handleError');
@@ -9,8 +10,6 @@ var randInt = require(__dirname + '/../lib/randInt');
 var percentCorrect = require(__dirname + '/../lib/percentcorrect');
 
 quotesRouter = exports = module.exports = express.Router();
-
-var choiceKey = {'Candidate': CandidateQuote, 'Dictator': DictatorQuote};
 
 quotesRouter.get('/', function(req, res) {
   var choice = randInt(0,2);
@@ -38,9 +37,9 @@ quotesRouter.post('/', bodyParser.json(), function (req, res){
 function total(quoteObj) {
   var correct = quoteObj.correctGuesses;
   var incorrect = quoteObj.incorrectGuesses;
-  var total = correct + incorrect;
+  var totalGuesses = correct + incorrect;
 
-  return total;
+  return totalGuesses;
 }
 
 quotesRouter.get('/stats', function(req, res) {
@@ -49,21 +48,18 @@ quotesRouter.get('/stats', function(req, res) {
     CandidateQuote.find({}, function(err, candidateData) {
       if (err) return handleError(err, res);
       var all = dictatorData.concat(candidateData);
+
       var correct = all.sort(function (a, b) {
         return b.correctGuesses - a.correctGuesses;
       });
+
       var incorrect = all.slice().sort(function (a, b) {
         return b.incorrectGuesses - a.incorrectGuesses;
-      });      // sorted.sort(function(a, b) {
-      //   if (percentCorrect(a) === 100 && percentCorrect(b) === 100) {
-      //     return total(b) - total(a);
-      //   } else {
-      //     return 0;
-      //   }
-      // });
+      });
 
       var results = {top: correct.slice(0, 10),
                      bottom: incorrect.slice(0, 10)};
+
       res.send(JSON.stringify(results));
     });
   });
--- test/test.js
+++ test/test.js
@@ -10,22 +10,23 @@ require(__dirname + '/../server');
 var CandidateQuote = require(__dirname + '/../models/candidateQuote');
 var DictatorQuote = require(__dirname + '/../models/dictatorQuote');
 
-describe('quotes GET & POST routes', function() {
+describe('quotesRouter.js', function() {
   after(function(done) {
     mongoose.connection.db.dropDatabase(function() {
       done();
     });
   });
 
-  beforeEach(function(done) {
+  before(function(done) {
     (new DictatorQuote({person: 'You', quote: 'Hate', category: 'Dictator', correctGuesses: 10, incorrectGuesses: 0})).save(function(err, data) {
       expect(err).to.eql(null);
       this.dictatorQuote = data;
       done();
     }.bind(this));
+
   });
 
-  beforeEach(function(done) {
+  before(function(done) {
     (new CandidateQuote({person: 'Me', quote: 'Love', category: 'Candidate', correctGuesses: 0, incorrectGuesses: 10})).save(function(err, data) {
       expect(err).to.eql(null);
       this.candidateQuote = data;
@@ -33,65 +34,87 @@ describe('quotes GET & POST routes', function() {
     }.bind(this));
   });
 
-  it('should GET either dictator or candidate quote', function(done) {
-    chai.request('localhost:3000')
-    .get('/api')
-    .end(function(err, res) {
-      if (JSON.parse(res.body).category === 'Dictator') {
+  describe("the '/' get route", function() {
+    it('should GET either dictator or candidate quote', function(done) {
+      chai.request('localhost:3000')
+      .get('/api')
+      .end(function(err, res) {
+        expect(res.status).to.eql(200);
         expect(err).to.eql(null);
-        expect(JSON.parse(res.body).person).to.eql('You');
-        expect(JSON.parse(res.body).quote).to.eql('Hate');
-        expect(JSON.parse(res.body)).to.have.property('_id');
-      }
-      if (JSON.parse(res.body).category === 'Candidate') {
+        if (JSON.parse(res.body).category === 'Dictator') {
+          expect(JSON.parse(res.body).person).to.eql('You');
+          expect(JSON.parse(res.body).quote).to.eql('Hate');
+          expect(JSON.parse(res.body)).to.have.property('_id');
+        }
+        if (JSON.parse(res.body).category === 'Candidate') {
+          expect(JSON.parse(res.body).person).to.eql('Me');
+          expect(JSON.parse(res.body).quote).to.eql('Love');
+          expect(JSON.parse(res.body)).to.have.property('_id');
+        }
+        done();
+      });
+    });
+  });
+
+  describe("the '/' POST route", function() {
+    it('should update (POST) correct guesses field in dictatorQuote db', function(done) {
+      chai.request('localhost:3000')
+      .post('/api')
+      .send(data = {quoteObj: this.dictatorQuote, answer: 'correct'})
+      .end(function(err, res) {
         expect(err).to.eql(null);
-        expect(JSON.parse(res.body).person).to.eql('Me');
-        expect(JSON.parse(res.body).quote).to.eql('Love');
-        expect(JSON.parse(res.body)).to.have.property('_id');
-      }
-      done();
+        expect(res.status).to.eql(200);
+        expect(res.body).to.be.a('object');
+        expect(res.body.msg).to.eql('update successful');
+        done();
+      });
+    });
+
+    it('should update (POST) incorrect guesses field in candidateQuote db', function(done) {
+      chai.request('localhost:3000')
+      .post('/api')
+      .send(data = {quoteObj: this.candidateQuote, answer: 'incorrect'})
+      .end(function(err, res) {
+        expect(err).to.eql(null);
+        expect(res.status).to.eql(200);
+        expect(res.body).to.be.a('object');
+        expect(res.body.msg).to.eql('update successful');
+        done();
+      });
     });
   });
 
-  it('should update (POST) correct guesses field in dictatorQuote db', function(done) {
-    chai.request('localhost:3000')
-    .post('/api')
-    .send(data = {quoteObj: this.dictatorQuote, answer: 'correct'})
-    .end(function(err, res) {
-      expect(err).to.eql(null);
-      expect(res.body).to.be.a('object');
-      expect(res.body.msg).to.eql('update successful');
-      done();
+  describe("the '/stats' GET route", function() {
+    it('should GET most correctly attributed quotes', function(done) {
+      var results = {top: this.dictatorQuote, bottom: this.candidateQuote};
+      chai.request('localhost:3000')
+      .get('/api/stats')
+      .send(results)
+      .end(function(err, res) {
+        var top = JSON.parse(res.text).top;
+        expect(err).to.eql(null);
+        expect(res.status).to.eql(200);
+        expect(top).to.be.a('array');
+        expect(top[0].correctGuesses).to.be.above(top[0].incorrectGuesses);
+        expect(top[0].quote).to.eql('Hate');
+        done();
+      });
+    });
+
+    it('should GET most incorrectly attributed quotes', function(done) {
+      var results = {top: this.dictatorQuote, bottom: this.candidateQuote};
+      chai.request('localhost:3000')
+      .get('/api/stats')
+      .send(results)
+      .end(function(err, res) {
+        var bottom = JSON.parse(res.text).bottom;
+        expect(err).to.eql(null);
+        expect(res.status).to.eql(200);
+        expect(bottom).to.be.a('array');
+        expect(bottom[0].incorrectGuesses).to.be.above(bottom[0].correctGuesses);
+        expect(bottom[0].quote).to.eql('Love');
+        done();
+      });
     });
   });
-
-  it('should update (POST) incorrect guesses field in candidateQuote db', function(done) {
-    chai.request('localhost:3000')
-    .post('/api')
-    .send(data = {quoteObj: this.candidateQuote, answer: 'incorrect'})
-    .end(function(err, res) {
-      expect(err).to.eql(null);
-      expect(res.body).to.be.a('object');
-      expect(res.body.msg).to.eql('update successful');
-      done();
-    });
-  });
-
-  it('should GET most correctly and incorrectly attributed quotes', function(done) {
-    var results = {top: this.dictatorQuote, bottom: this.candidateQuote};
-    chai.request('localhost:3000')
-    .get('/api/stats')
-    .send(results)
-    .end(function(err, res) {
-      expect(err).to.eql(null);
-      expect(JSON.parse(res.text).top).to.be.a('array');
-      expect(JSON.parse(res.text).top[0].correctGuesses).to.be.above(JSON.parse(res.text).top[0].incorrectGuesses);
-      expect(JSON.parse(res.text).top[0].quote).to.eql('Hate');
-      expect(JSON.parse(res.text).bottom).to.be.a('array');
-      expect(JSON.parse(res.text).bottom[0].incorrectGuesses).to.be.above(JSON.parse(res.text).bottom[0].correctGuesses);
-      expect(JSON.parse(res.text).bottom[0].quote).to.eql('Love');
-      done();
-    });
-  });
-
 });
